ee60c8c49cd16ad302de3085fb8a9c228e7b98f5
Razvan Trufan <razvan.trufan@amdaris.com>
2023-05-10 10:49:59 +0300

    Added the Variables and Comments Task

---
+++ src/main/java/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,48 @@
+package variablesandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+        // Declararea variabilelor - tipuri de date primitive in Java
+
+        boolean falseBooleanVariable; // declarare variabila de tip boolean - false
+        boolean trueBooleanVariable; // declarare variabila de tip boolean - true
+        char firstCharFromAscii; // declarare variabila de tip char - prima valoare din Ascii table
+        char lastCharFromAscii; // declarare variabila de tip char - ultima valoare din Ascii table
+        byte lowerLimitOfByteDataType; // declarare variabila de tip byte - first value
+        byte upperLimitOfByteDataType; // declarare variabila de tip byte - last value
+        short lowerLimitOfShortDataType; // declarare variabila de tip short - first value
+        short upperLimitOfShortDataType; // declarare variabila de tip short - last value
+        int lowerLimitOfIntDataType; // declarare variabila de tip int - first value
+        int upperLimitOfIntDataType; // declarare variabila de tip int - last value
+        long lowerLimitOfLongDataType; // declarare variabila de tip long - first value
+        long upperLimitOfLongDataType; // declarare variabila de tip long - last value
+        float lowerLimitOfFloatDataType; // declarare variabila de tip float - first value
+        float upperLimitOfFloatDataType; // declarare variabila de tip float - last value
+        double lowerLimitOfDoubleDataType; // declarare variabila de tip double - first value
+        double upperLimitOfDoubleDataType; // declarare variabila de tip double - last value
+
+        // Initializarea fiecarei variabile anterioare
+
+        falseBooleanVariable = false; // initializare variabila de tip boolean - false
+        trueBooleanVariable = true; // initializare variabila de tip boolean - true
+        firstCharFromAscii = 'a'; // initializare variabila de tip char - prima valoare din Ascii table
+        lastCharFromAscii = 'Z'; // initializare variabila de tip char - ultima valoare din Ascii table
+        lowerLimitOfByteDataType = -128; // initializare variabila de tip byte - first value
+        upperLimitOfByteDataType = 127; // initializare variabila de tip byte - last value
+        lowerLimitOfShortDataType = -32768; // initializare variabila de tip short - first value
+        upperLimitOfShortDataType = 32767; // initializare variabila de tip short - last value
+        lowerLimitOfIntDataType = -2147483648; // initializare variabila de tip int - first value
+        upperLimitOfIntDataType = 2147483647; // initializare variabila de tip int - last value
+        lowerLimitOfLongDataType = -9223372036854775808L; // initializare variabila de tip long - first value
+        upperLimitOfLongDataType = 9223372036854775807L; // initializare variabila de tip long - last value
+        lowerLimitOfFloatDataType = -3.4E+38f; // initializare variabila de tip float - first value
+        upperLimitOfFloatDataType = 3.4E+38f; // initializare variabila de tip float - last value
+        lowerLimitOfDoubleDataType = -1.7E+308d; // initializare variabila de tip double - first value
+        upperLimitOfDoubleDataType = 1.7E+308d; // initializare variabila de tip double - last value
+        {
+
+            System.out.println( falseBooleanVariable );
+        }
+    }
+}
