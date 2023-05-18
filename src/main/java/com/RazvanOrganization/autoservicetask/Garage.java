0550544e6ab5d63c05770c83383daf1333237dd5
Razvan Trufan <razvan.trufan@amdaris.com>
2023-05-19 00:39:57 +0300

    Added the Java Code for an Auto Service app.

---
+++ src/main/java/com/RazvanOrganization/autoservicetask/Garage.java
@@ -0,0 +1,19 @@
+package com.RazvanOrganization.autoservicetask;
+
+public class Garage {
+    String address;
+    String numberId;
+    boolean hasComputerDiagnostics;
+    int carCapacity;
+    String scheduler;
+
+    Garage(String address) {
+        this.address = address;
+    }
+
+    Garage(int carCapacity) {
+        this.carCapacity = carCapacity;
+    }
+
+
+}
---
+++ src/main/java/com/RazvanOrganization/autoservicetask/Worker.java
@@ -0,0 +1,12 @@
+package com.RazvanOrganization.autoservicetask;
+
+public class Worker {
+    String name;
+    double height;
+    double weight;
+    int age;
+    String sexType;
+    String specialization;
+    boolean isExperienced;
+
+}
