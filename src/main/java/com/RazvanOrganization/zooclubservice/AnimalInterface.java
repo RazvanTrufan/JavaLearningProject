9ee62bdb4ad13414e2d2f921edd753cb756e8a58
Razvan Trufan <razvan.trufan@amdaris.com>
2023-05-29 18:21:29 +0300

    Added the implementation of Zoo Service by using OOP.

---
+++ src/main/java/com/RazvanOrganization/zooclubservice/AnimalInterface.java
@@ -0,0 +1,9 @@
+package com.RazvanOrganization.zooclubservice;
+
+public interface AnimalInterface {
+
+    //Toate Clasele care implementeaza AnimalInterface vor trebui sa faca override la makesound() -> adica sa implementeze metoda
+
+    void makeSound();
+
+}
